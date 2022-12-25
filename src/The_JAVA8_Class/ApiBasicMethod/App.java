2e0c759d39f5a89aac03bd6feb030287c8c4a925
HanSeongDeok <hsd970730@naver.com>
2022-12-25 20:09:06 +0900

    API의 기본 메소드와 스태틱 메소드

--- src/The_JAVA8_Class/ApiBasicMethod/App.java
+++ src/The_JAVA8_Class/ApiBasicMethod/App.java
@@ -1,2 +1,49 @@
-package The_JAVA8_Class.ApiBasicMethod;public class App {
+package The_JAVA8_Class.ApiBasicMethod;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class App{
+    public static void main(String[] args) {
+        List<String> name = new ArrayList<>();
+        name.add("seongdeok");
+        name.add("keesun");
+        name.add("foo");
+        name.add("todo");
+
+        /**
+         * 같은 의미 다른표현 ::아래::
+         */
+        for(String str_name : name){
+            System.out.println(str_name);
+        }
+
+        name.forEach(s -> {
+            System.out.println(s);
+        });
+
+        name.forEach(System.out::println);
+
+        /**
+         * spliterator
+         */
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator2 = spliterator.trySplit();
+        Spliterator<String> spliterator3 = name.spliterator();
+
+        System.out.println("=============================");
+        while (spliterator.tryAdvance(System.out::println));         //tryAdvance;
+        System.out.println("=============================");
+        while (spliterator2.tryAdvance(System.out::println));
+        //spliterator.tryAdvance(x -> System.out.println(x));
+        /*while (spliterator3.tryAdvance(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        }));*/
+        System.out.println("=============================");
+        spliterator3.forEachRemaining(System.out::println);
+
+    }
 }
