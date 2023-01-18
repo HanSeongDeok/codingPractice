e8f35a4de63ed97b0299bc5c2094a85f8e164a91
HanSeongDeok <hsd970730@naver.com>
2023-01-18 23:24:18 +0900

    LinkedList_Practice_Review

--- src/LifeCoding_Class/Collection_FrameWork/LinkedList_Review2.java
+++ src/LifeCoding_Class/Collection_FrameWork/LinkedList_Review2.java
@@ -3,13 +3,17 @@ package LifeCoding_Class.Collection_FrameWork;
 public class LinkedList_Review2 {
     Node head;
     Node tail;
-    public class Node{
+    static public class Node{
         int data;
         Node next;
         Node(int data){
             this.data =data;
         }
+        public String toString(){
+            return String.valueOf(this.data);
+        }
     }
+
     //addFirst
     void addFirst(int data){
         Node n = new Node(data);
@@ -27,10 +31,71 @@ public class LinkedList_Review2 {
             tail = n;
         }
     }
+    Node node (int index){
+        Node x = head;
+        for(int i=0; i<index; i++){
+            x = x.next;
+        }
+        return x;
+    }
+    public void add(int k, int input){
+        if(k == 0){
+            addFirst(input);
+        } else {
+           Node temp1 = node(k-1);
+           Node temp2 = temp1.next;
+           Node newNode = new Node(input);
+           temp1.next = newNode;
+           newNode.next = temp2;
+           if(newNode.next==null){
+               tail = newNode;
+           }
+        }
+    }
+    public String toString(){
+        if(head == null){
+            return "[]";
+        }
+        Node temp = head;
+        String str = "";
+        while (temp.next != null){
+            str += temp.data + " -> ";
+            temp = temp.next;
+        }
+        str += temp.data;
+        return str;
+    }
+    public Object removeFirst(){
+        Node temp = head;
+        head = head.next;
+        Object returnData = temp.data;
+        temp = null;
+        return returnData;
+    }
+
+    public Object remove(int k) {
+        if(k == 0) {
+            return removeFirst();
+        }
+        Node temp = node(k-1);
+        Node deleteNode = temp.next;
+        int returnData = deleteNode.data;
+        temp.next = temp.next.next;
+        if(deleteNode.equals(tail)){
+            tail = temp;
+        }
+
+        return returnData;
+    }
+
     public static void main(String[] args) {
         LinkedList_Review2 ll = new LinkedList_Review2();
         ll.addLast(1);
         ll.addLast(2);
         ll.addLast(3);
+        ll.addLast(4);
+        ll.add(2,5);
+        System.out.println(ll.removeFirst());
+        System.out.println(ll);
     }
 }
