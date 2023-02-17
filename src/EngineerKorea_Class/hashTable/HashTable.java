8f51268bba12eb93cccebf46d0e240f494b228d5
HanSeongDeok <hsd970730@naver.com>
2023-02-17 23:53:42 +0900

    UnderStand Multi Thread

---
+++ src/EngineerKorea_Class/hashTable/HashTable.java
@@ -0,0 +1,7 @@
+package EngineerKorea_Class.hashTable;
+
+public class HashTable {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ThreadPractice/TreadMain.java
@@ -0,0 +1,10 @@
+package ThreadPractice;
+
+public class TreadMain{
+    public class WorkerThread extends Thread implements Runnable{};
+    public static void main(String[] args) {
+        Thread thread = new Thread(()->{
+           //Nothing
+        });
+    }
+}
